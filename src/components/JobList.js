419a1a10399f09f466d4a80cd1b54e869e7146f3
Prasitha7 <prasithajeevadya@gmail.com>
2024-08-01 11:35:26 +0530

    Added Pagination to Jobs Page

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { getCompanyLogo } from '../utils/getCompanyLogo';
 
-import Box from '@mui/material/Box';
+//Material UI components
+import Box from '@mui/material/Box'; 
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
 import { Avatar, ListItemAvatar, Typography } from '@mui/material';
+import Pagination from '@mui/material/Pagination'; 
+import Stack from '@mui/material/Stack';
 
 const JobList = ({ jobs = [], onJobSelect }) => {
+  const [page, setPage] = useState(1); // State to manage current page
+  const itemsPerPage = 5; // Define number of items per page
+
+  const handlePageChange = (event, value) => {
+    setPage(value); // Update page state when pagination changes
+  };
+
+  const paginatedJobs = jobs.slice((page - 1) * itemsPerPage, page * itemsPerPage);
 
   return (
     <div className="job-list-container">
-      <h1 className="title">Jobs Found</h1>
+      <Stack spacing={2} direction="row" justifyContent="center" alignItems="center" sx={{ marginTop: 2 }}>
+          <Pagination 
+            hidePrevButton
+            hideNextButton
+            count={Math.ceil(jobs.length / itemsPerPage)} 
+            page={page} 
+            onChange={handlePageChange} 
+          />
+      </Stack>
       <div className="job-list">
         <div className="job-list-sidebar">
-          {jobs.map(job => (
+          {paginatedJobs.map(job => (
             <List key={job.id} sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
               <ListItemButton onClick={() => onJobSelect(job)}>
                 <ListItemAvatar>
@@ -42,6 +60,7 @@ const JobList = ({ jobs = [], onJobSelect }) => {
             </List>
           ))}
         </div>
+        
       </div>
     </div>
   );
