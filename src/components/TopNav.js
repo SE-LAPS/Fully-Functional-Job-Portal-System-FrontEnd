c9fc60a8809d174562b64cec8f7e24a71a7e9d62
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-08-06 15:43:18 +0530

    Update Want a Job

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,4 +1,3 @@
-// TopNav.js
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../css/TopNav.css';
@@ -6,28 +5,36 @@ import logoImage from '../assets/companyLogos/2.png';
 
 const TopNav = () => {
   const navigate = useNavigate();
-  
+
   const handlePostJobClick = () => {
     navigate('/post-job');
+  };
+
+  const handleWantJobClick = () => {
+    navigate('/want-job');
   };
 
   return (
     <nav className="top-nav">
       <div className="logo-container">
-        <div className="icon-container">
-          
-        </div>
+        <div className="icon-container"></div>
         <img src={logoImage} alt="Company Logo" className="company-logo" />
       </div>
       <div className="nav-buttons">
         <div className="top-nav-buttons">
-          <Link to="/login" className="btn-shine">LOGIN / SIGNUP</Link>
+          <Link to="/login" className="btn-shine">Register / Login</Link>
         </div>
         <button className="top-nav-btn post-job-btn" onClick={handlePostJobClick}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 24">
             <path d="m18 0 8 12 10-8-4 20H4L0 4l10 8 8-12z"></path>
           </svg>
           POST A JOB
+        </button>
+        <button className="top-nav-btn post-job-btn" onClick={handleWantJobClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 24">
+            <path d="m18 0 8 12 10-8-4 20H4L0 4l10 8 8-12z"></path>
+          </svg>
+          WANT A JOB
         </button>
       </div>
     </nav>
